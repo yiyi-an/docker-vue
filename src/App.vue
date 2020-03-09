99ee2f1e2ae67bd36d2d4dc171d29c0db0091b5d
安晓东 <xiaodong.an@zhaopin.com.cn>
2020-03-09 15:17:32 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" style="width:120px;height:120px" src="./assets/logo.png">
+    <HelloWorld msg="vue+Docker+Nginx 一键部署"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,44 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="content">
+      <h3>使用流程:</h3>
+      <ol>
+        <li>git拉取代码到本地<br>
+          <a href="https://github.com/yiyi-an/docker-vue">
+           https://github.com/yiyi-an/docker-vue.git
+          </a>
+        </li>
+        <li>coding...</li>
+        <li>将代码提交至远程仓库 git </li>
+        <li>登录jenkins:
+            <ul>
+              <li>
+                <a href="http://jenkins.dodolib.cn">http://jenkins.dodolib.cn</a>
+              </li>
+              <li>
+                 或者 <a href="http://123.206.204.35:8080">http://123.206.204.35:8080</a>
+              </li>
+              <li>账号/密码: anxiaodong</li>
+            </ul>
+        </li>
+        <li>选择项目: node-vuedemo-web</li>
+        <li>立即构建</li>
+        <li>构建完成</li>
+        <li>访问地址: 
+          <ul>
+            <li>
+              <a href="http://vuedocer.dodolib.cn/"> http://vuedocer.dodolib.cn/</a>
+            </li>
+             <li>
+              或者 <a href="http://vuedocer.dodolib.cn/"> http://123.206.204.35:10002</a>
+            </li>
+          </ul>
+        </li>
+      </ol>
+    </div>
+    
+   
   </div>
 </template>
 
@@ -41,18 +53,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
+}
+.content{
+  margin: 0 auto;
+  text-align: left;
+  width:500px;
+  border:2px solid #666;
+  padding: 10px;
+  background-color:aquamarine;
+  border-radius: 5px;
 }
 </style>
