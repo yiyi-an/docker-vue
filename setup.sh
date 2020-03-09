c8927400f998d3aab085525f4291254065f1a55e
外星人 <anxiaodong@123.com>
2020-03-09 14:36:23 +0800

    构建相关

---
+++ setup.sh
@@ -0,0 +1,16 @@
+image_version=`date +%Y%m%d%H%M`;
+echo 开始构建:${image_version};
+# cd vue2docker
+#cd project;
+git pull --rebase origin master;
+#cd ../;
+echo 停止vue2docker;
+docker stop vue2docker;
+docker rm vue2docker;
+docker build -t vue2docker:$image_version .;
+docker images;
+docker run -p 10002:80 -d --name vue2docker vue2docker:$image_version;
+# -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
+docker logs vue2docker;
+#删除build过程中产生的镜像    #docker image prune -a -f
+docker rmi $(docker images -f "dangling=true" -q)
