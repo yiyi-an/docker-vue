eecca19082d1d6a89c9361ee75be2782bb99dc62
安晓东 <xiaodong.an@zhaopin.com.cn>
2020-03-09 18:42:34 +0800

    更新视图

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" style="width:120px;height:120px" src="./assets/logo.png">
-    <HelloWorld msg="vue+Docker+Nginx 一键部署"/>
+    <h2>{{title}}</h2>
+     <div class="btn-wrap">
+      <a  href="#" @click="componentId='HelloWorld'">使用流程</a>
+      <a href="http://blog.dodolib.cn">搭建文档</a>
+    </div>
+    <component :is="componentId"></component>
   </div>
 </template>
 
@@ -12,17 +17,31 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      title:"Vue+Docker+Nginx 一键部署方案",
+      componentId:''
+    }
   }
 }
 </script>
 
 <style>
 #app {
+  width:500px;
+  margin:0 auto;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+}
+.btn-wrap{
+  margin-bottom: 20px;
+}
+.btn-wrap a{
+  margin-right: 20px;
 }
 </style>
